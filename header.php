3131f22d99e5b61c04fe1a3c0ec2af1634b74d6c
HeeWon Lee <ncross42@gmail.com>
2016-08-02 12:20:57 +0900

    added home link

--- header.php
+++ header.php
@@ -29,7 +29,9 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-header-main">
 				<div class="site-branding">
-					<?php twentysixteen_the_custom_logo(); ?>
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <?php twentysixteen_the_custom_logo(); ?>
+          </a>
 
 					<?php if ( is_front_page() && is_home() ) : ?>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
